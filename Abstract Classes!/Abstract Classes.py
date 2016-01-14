a6afab1910aee58b5a2156742e057c5c0aa4aca3
youfeng <youfeng243@163.com>
2016-01-14 08:12:51 +0800

    Day13 Hackerrank

---
+++ Abstract Classes!/Abstract Classes.py
@@ -0,0 +1,30 @@
+from abc import ABCMeta, abstractmethod
+class Book:
+    __metaclass__ = ABCMeta
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): 
+        pass
+    
+#Write MyBook class
+class MyBook( Book ):
+    def __init__( self, title, author, price ):
+        super(MyBook, self).__init__(title, author)
+        
+        self.title=title
+        self.author=author   
+        self.price = price
+        
+        
+    def display(self):
+        print "Title:", self.title
+        print "Author:", self.author
+        print "Price:", self.price
+        
+title=raw_input()
+author=raw_input()
+price=int(raw_input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
