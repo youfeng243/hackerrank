765725887df696652924adb9fdbf1a429d2cf05e
youfeng <youfeng243@163.com>
2016-01-19 22:29:52 +0800

    bit

--- Cipher/Cipher.py
+++ Cipher/Cipher.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 N,K = map(int, raw_input().strip().split())
 binary = map(int, list(raw_input().strip()))
 
@@ -7,55 +9,50 @@ prelength = K - 1
 answer = []
 start = 0
 end = 0
-dic = {}
-dic[0] = 0
-dic[1] = 0
+dic = [0, 0]
 
 for i in xrange( length ):
-
-    print "i = %d dic[0] + dic[1] = %d" % (i, dic[0] + dic[1])
-
-    if start == 0 and end == 0:
+    
+    if i == 0:
         answer.append(binary[i])
         dic[binary[i]] += 1
-        end += 1
         if i >= K - 1:
-            dic[binary[start]] -= 1
+            dic[answer[start]] -= 1
             start += 1        
         continue
 
     if dic[0] % 2 == 0 and dic[1] % 2 == 0:
         answer.append(binary[i])
         dic[binary[i]] += 1
-        
-        end += 1
         if i >= K - 1:
-            dic[binary[start]] -= 1
+            dic[answer[start]] -= 1
             start += 1
         continue
+        
     if dic[0] % 2 == 0 and dic[1] % 2 == 1:
         answer.append( binary[i] ^ 1 )
         dic[binary[i] ^ 1] += 1
+        if i >= K - 1:
+            dic[answer[start]] -= 1
+            start += 1
+        continue
         
-        end += 1
-        if i >= K - 1:
-            dic[binary[start]] -= 1
-            start += 1
-        continue
     if dic[0] % 2 == 1 and dic[1] % 2 == 0:
-        answer.append( binary[i] ^ 0 )
-        dic[binary[i] ^ 0] += 1
-        end += 1
+        answer.append( binary[i] )
+        dic[binary[i]] += 1
         if i >= K - 1:
-            dic[binary[start]] -= 1
+            dic[answer[start]] -= 1
             start += 1
         continue
+        
     if dic[0] % 2 == 1 and dic[1] % 2 == 1:
         answer.append( binary[i] ^ 1 )
         dic[binary[i] ^ 1] += 1
-        end += 1
         if i >= K - 1:
-            dic[binary[start]] -= 1
+            dic[answer[start]] -= 1
             start += 1
         continue
-print answer
+for i in xrange( length ):
+    print( answer[i], sep="", end="" )
+print ("")
+    #print answer
