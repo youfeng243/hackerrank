79308b39871c45ef3e3e03256871475ec5ab1311
youfeng <youfeng243@163.com>
2016-02-16 08:18:49 +0800

    哈夫曼解码

--- Domains/Data Structures/Tree Huffman Decoding/Tree Huffman Decoding.cpp
+++ Domains/Data Structures/Tree Huffman Decoding/Tree Huffman Decoding.cpp
@@ -14,7 +14,30 @@ typedef struct node
 */
 
 
-void decode_huff(node * root,string s)
-{
+void decode_huff(node * root,string s){
+    if( root == NULL ){
+        return;
+    }
+    int i = 0;
+    int length = s.length();
     
+    while( i < length ){
+        node *point = root;
+        while( i < length ){
+            if( s[i] == '1' ){
+                point = point->right;
+            }
+            else{
+                point = point->left;
+            }
+            i++;
+            
+            if( point->left == NULL && point->right == NULL ){
+                cout<<point->data;
+                break;
+            }
+            
+        }
+    }
+    cout<<endl;
 }
